1b10d79f9e43437d298b0a4a3f6815822efac26e
Timo Klein <tkleia34@gmail.com>
2019-05-29 18:33:02 +0200

    add convert npy to torch tensor script

--- car_racer/perception/converters.py
+++ car_racer/perception/converters.py
@@ -12,21 +12,46 @@ def convert_to_tensor(path: pathlike) -> None:
     """
     Function for converting saved ndarrays (.npy files) to torch tensors.
     Tensors will be saved in a subdirectory called 'tensor_files'.
+    Array shapes: (N,H,W,3)
+    Tensor shapes: (N,1,H,W)
     INPUT: path to saved ndarrays
     """
     path = Path(path)
-    save_dir = path/"tensor_files"
-    for array in path.glob("*.npy").iterdir():
-        np.load(str(array))
+    # convert arrays in target folder to tensors and save
+    for array in path.glob("*.npy"):
+        image_batch = np.load(str(array))
 
+        # leave the first 40 frames out as they contain zoomed images
+        converted = torch.from_numpy(image_batch[40:, ...])
+        fp = path/array.stem
+        torch.save(converted, fp.with_suffix(".pt"))
 
 
 def convert_to_grayscale(path: pathlike) -> None:
     """
     Converts RGB image samples in torch tensors to grayscale.
     Tensors should have the dimensions (N, H, W, 3).
-    Saved tensors will have the form (N, H, W, 1).
+    Saved tensors will have the form (N, 1, H, W).
     INPUT: path to saved torch tensors
     """
-    pass
+    # grayscaling pipeline using torch transforms
+    grayscaler = T.Compose([T.ToPILImage(),
+                        T.Grayscale(num_output_channels=1),
+                        T.ToTensor()])
 
+    path = Path(path)
+    savedir = path/"grayscale"
+
+    for batch in path.glob("*.pt"):
+        tensor = torch.load(str(batch))
+        for row in tensor:
+            converted = grayscaler(row)
+
+
+def main():
+    path = "/home/timo/DataSets/carracer_images/"
+    convert_to_tensor(path)
+
+
+if __name__ == '__main__':
+        main()
