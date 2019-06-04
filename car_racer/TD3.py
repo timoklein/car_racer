5d3fab26edfc2ab4915a2c47c2e06ea4637996cc
Timo Klein <tkleia34@gmail.com>
2019-06-04 17:54:56 +0200

    added utils.py

--- car_racer/TD3.py
+++ car_racer/TD3.py
@@ -145,4 +145,4 @@ class TD3(object):
 
 	def load(self, filename, directory):
 		self.actor.load_state_dict(torch.load('%s/%s_actor.pth' % (directory, filename)))
-self.critic.load_state_dict(torch.load('%s/%s_critic.pth' % (directory, filename)))
+        self.critic.load_state_dict(torch.load('%s/%s_critic.pth' % (directory, filename)))
