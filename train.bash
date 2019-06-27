2bbce07f01fb40ba627b5f318007edffa0f682a3
Timo Klein <tkleia34@gmail.com>
2019-06-27 08:05:50 +0200

    deleted train.bash

--- train.bash
+++
@@ -1,6 +0,0 @@
-echo "Starting training..."
-# on cloud:
-xvfb-run -a -s "-screen 0 1400x900x24 +extension RANDR" -- python train_td3.py &
-# on macbook for debugging:
-#python python train_td3.py&
-sleep 1.0
